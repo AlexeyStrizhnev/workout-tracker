0ee4c308ea05fe1fc7419955f3cb508bb1effc5e
AlexeyStrizhnev <alexey.strizhnev@gmail.com>
2025-09-17 13:50:33 +0700

    Добавлены основные файлы

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'workout-tracker-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/chart.js',
+  '/chart.umd.js.map',
+  '/icon-192x192.png',
+  '/icon-512x512.png'
+];
+
+// Установка Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Активация Service Worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Перехват запросов
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
